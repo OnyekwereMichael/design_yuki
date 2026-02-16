53a6766a01fa830c5aa01f3bd6b739890645a66f
OnyekwereMichael <onyekweremichael55@gmail.com>
2026-02-16 17:37:41 +0100

    feat: add Hero component displaying profile image, headline, description, and CTA buttons.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { icons } from '../../constants';
 
 const Hero = () => {
   return (
-    <section className=" flex items-center justify-center pt-32 2xl:pt-[170px] 2xl:mt-64">
+    <section className=" flex items-center justify-center pt-32 2xl:pt-[170px] 2xl:mt-56">
 
       <div className=" mx-auto text-center flex flex-col items-center">
         {/* Profile Image */}
